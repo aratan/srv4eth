c4594bb6898c6b4f40929b4469e27d20ce40a3d9
Hid2enTech <systemdeveloper868@gmail.com>
2020-01-26 13:56:40 +0100

    La conversion no funciona aun, todas las funciones estan integradas excepto mod valor en contrato y web es básica

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import(
 	"fmt"
-	"github.com/aratan/srv4eth/cuentas"
+/*	"github.com/aratan/srv4eth/cuentas"
 	"github.com/aratan/srv4eth/saldo"
 	"github.com/aratan/srv4eth/envio"
+*/
+	"github.com/aratan/srv4eth/web"  // TODO se complica
 	//"github.com/aratan/srv4eth/conversion"
 )
 
 func main() {
+	/*
 	fmt.Println("Cuenta:..........")
 	fmt.Println(cuentas.Show())
 	fmt.Println("Saldo:...........")
@@ -18,5 +21,6 @@ func main() {
 
 	fmt.Println("Saldo:...........")
 	fmt.Println(saldo.Show())
-	//fmt.Println(conversion.Show())
+	*/
+	fmt.Println(web.Show())
 }
