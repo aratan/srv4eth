f84ca3cc838cd4a0afbf5207d9c51f143c68d6b1
Hid2enTech <systemdeveloper868@gmail.com>
2020-01-26 14:15:42 +0100

    GET ok

--- web/web.go
+++ web/web.go
@@ -2,42 +2,45 @@ package web
 
 import (
 	"github.com/aratan/srv4eth/cuentas"
-	"fmt"
+	//"fmt"
 	"io"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
+	//"html/template"
 	//"strconv"
 	"time"
 )
 
+
+
+
+func Gethandler(w http.ResponseWriter, r *http.Request) {
+	var cuenta string
+	cuenta = "Mi cuenta: " + cuentas.Show()
+	//cuenta := cuentas.Show()
+	io.WriteString(w, cuenta)
+}
+
+
 func Show() string {
-	fmt.Println("80")
-	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hola")
+	// Ruteador
+	r := mux.NewRouter().StrictSlash(false)
+	// Rutas
+	r.HandleFunc("/", Gethandler).Methods("GET")
 
-	})
 
-	http.HandleFunc("/", handler)
-	// Se cambia http.ListenAndServe(":80",nil) por lo siguiente:
 	// Configuracion servicio web
 	server := &http.Server{
 		Addr:           ":80",
+		Handler:		r,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-		// 1 mega
+		MaxHeaderBytes: 1 << 20,  // 1 mega
 	}
+
 	//activa servidor web
 	log.Println("Escuchando 80")
 	log.Fatal(server.ListenAndServe())
-	return "aaa"
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	var cuenta string
-	cuenta = cuentas.Show() + "Mi cuenta"
-	//cuenta := cuentas.Show()
-	
-	io.WriteString(w, cuenta)
-	
+	return ""
 }
